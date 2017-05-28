2ed0d71dc0786ad61dc5ee569c020fda44716f57
Mick MacCallum <micksmaccallum@gmail.com>
2017-05-28 15:43:27 -0400

    Initial Commit

---
+++ Wut Do?/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Wut Do?
+//
+//  Created by Michael MacCallum on 5/28/17.
+//  Copyright © 2017 Robyn Cute. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
